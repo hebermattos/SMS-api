6118384756e849b1cb0300944b9cd2bf49b92735
heber mattos <feanorkun@hotmail.com>
2026-09-17 21:14:10 -0300

    Remove migration-based database tooling

--- database/migrations/006_shared_provider_account_routing.sql
+++
@@ -1,9 +0,0 @@
-UPDATE dbo.TenantSmsProviders
-SET FromNumber = LTRIM(RTRIM(FromNumber))
-WHERE FromNumber IS NOT NULL;
-GO
-
-CREATE UNIQUE INDEX UX_TenantSmsProviders_CallbackRoute
-    ON dbo.TenantSmsProviders(Provider, AccountId, FromNumber)
-    WHERE IsActive = 1 AND FromNumber IS NOT NULL;
-GO
