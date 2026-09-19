e738ad2b4abab1038c7db0dc6b135018cd45f786
heber mattos <feanorkun@hotmail.com>
2026-09-19 20:22:24 -0300

    Register PostgreSQL DateTimeOffset Dapper handler

--- src/Sms.Infrastructure/Persistence/SqlConnectionFactory.cs
+++ src/Sms.Infrastructure/Persistence/SqlConnectionFactory.cs
@@ -1,12 +1,18 @@
+using System.Data;
+using Dapper;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
-using System.Data;
 
 namespace Sms.Infrastructure.Persistence;
 
 public sealed class SqlConnectionFactory
 {
     private readonly string _connectionString;
+
+    static SqlConnectionFactory()
+    {
+        SqlMapper.AddTypeHandler(new PostgresDateTimeOffsetHandler());
+    }
 
     public SqlConnectionFactory(IConfiguration configuration)
     {
