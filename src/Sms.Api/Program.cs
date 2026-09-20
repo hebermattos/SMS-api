cc81f539b37a054358b9577cbc382b186d3584af
heber mattos <feanorkun@hotmail.com>
2026-09-20 19:22:45 -0300

    Register Redis rate limit counter

--- src/Sms.Api/Program.cs
+++ src/Sms.Api/Program.cs
@@ -20,6 +20,7 @@ using Sms.Application;
 using Sms.Application.Common;
 using Sms.Infrastructure;
 using Sms.Infrastructure.Observability;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.AddSimpleConsole(options =>
@@ -94,6 +95,10 @@ builder.Services.AddRateLimiter(options =>
 });
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis")
+    ?? throw new InvalidOperationException("Connection string 'Redis' is required for distributed rate limiting.");
+builder.Services.AddSingleton<IConnectionMultiplexer>(_ => ConnectionMultiplexer.Connect(redisConnectionString));
+builder.Services.AddSingleton<IRateLimitCounter, RedisRateLimitCounter>();
 builder.Services.AddDependencyHealthChecks(builder.Configuration);
 
 var app = builder.Build();
