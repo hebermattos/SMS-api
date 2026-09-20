0491a47cba96563ab897ead8728bcf19d5762009
heber mattos <feanorkun@hotmail.com>
2026-09-20 09:59:30 -0300

    Use disabled cache proxy in dependency injection

--- src/Sms.Infrastructure/DependencyInjection.cs
+++ src/Sms.Infrastructure/DependencyInjection.cs
@@ -44,7 +44,7 @@ public static class DependencyInjection
         }
         else
         {
-            services.AddSingleton<IDistributedCache, DisabledDistributedCache>();
+            services.AddSingleton<IDistributedCache, DisabledDistributedCacheProxy>();
         }
 
         services.AddSingleton<SqlConnectionFactory>();
