dbf7e984cdad8a02d576fc79e1b83f6d94cb4017
heber mattos <feanorkun@hotmail.com>
2026-09-20 10:15:02 -0300

    Fix seed cache constructor
    Remove obsolete CacheOptions usage from the seed project and update TenantConfigurationCache construction.

--- database/seeds/Sms.Seed/Program.cs
+++ database/seeds/Sms.Seed/Program.cs
@@ -6,7 +6,6 @@ using Sms.Infrastructure.Persistence;
 using Sms.Infrastructure.Security;
 using Sms.Seed;
 using Sms.Application.Auth;
-using Sms.Infrastructure.Caching;
 
 var configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
 var connectionFactory = new SqlConnectionFactory(configuration);
@@ -34,7 +33,6 @@ Console.WriteLine("Initial platform administrator is configured. Existing passwo
 var configurationCache = new TenantConfigurationCache(
     connectionFactory,
     new SeedDistributedCache(),
-    CacheOptions.From(configuration),
     NullLogger<TenantConfigurationCache>.Instance);
 var providers = new TenantSmsProviderRepository(
     connectionFactory,
