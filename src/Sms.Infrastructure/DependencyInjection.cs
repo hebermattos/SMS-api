46d9be4cd8ed375d7a36483ee5ef81431d8705f4
heber mattos <feanorkun@hotmail.com>
2026-09-20 16:02:39 -0300

    Unify outbound SMS queue publishing

--- src/Sms.Infrastructure/DependencyInjection.cs
+++ src/Sms.Infrastructure/DependencyInjection.cs
@@ -53,7 +53,7 @@ public static class DependencyInjection
         services.AddSingleton<TenantConfigurationCache>();
         services.AddSingleton<IProviderCatalogCache, ProviderCatalogCache>();
         services.AddSingleton<ITenantSmsOverviewOutbox, TenantSmsOverviewOutbox>();
-        services.AddSingleton<IFailedSmsPublishSource, FailedSmsPublishSource>();
+        services.AddSingleton<ISmsQueuePublishSource, SmsQueuePublishSource>();
         services.AddSingleton<ITenantSmsOverviewEventPublisher, TenantSmsOverviewEventPublisher>();
         services.AddScoped<ITenantSmsOverviewProjection, TenantSmsOverviewProjection>();
         var rabbitMq = RabbitMqAlertOptions.From(configuration);
