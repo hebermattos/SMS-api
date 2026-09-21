35061bdba95945df60138a7a92ab9fa0368d457d
heber mattos <feanorkun@hotmail.com>
2026-09-21 15:59:30 -0300

    Use cursor pagination for log queries

--- src/Sms.Infrastructure/Sql/Persistence/LogEntryRepository.GetActivityAsync.01.sql
+++ src/Sms.Infrastructure/Sql/Persistence/LogEntryRepository.GetActivityAsync.01.sql
@@ -3,5 +3,6 @@ FROM UserActivityLogs
 WHERE TenantId=@TenantId
   AND (@From IS NULL OR "Timestamp" >= @From)
   AND (@To IS NULL OR "Timestamp" < @To)
+  AND (@CursorTimestamp IS NULL OR "Timestamp" < @CursorTimestamp OR ("Timestamp" = @CursorTimestamp AND Id < @CursorId))
 ORDER BY "Timestamp" DESC, Id DESC
-OFFSET @Skip LIMIT @Take;
+LIMIT @Take;
