bdea3852d524eb785cee5da88d98b950737966d5
heber mattos <feanorkun@hotmail.com>
2026-09-21 15:07:51 -0300

    Remove polling-style alert window query

--- src/Sms.Infrastructure/Sql/Persistence/AlertRepository.EvaluateAsync.05.sql
+++
@@ -1,7 +0,0 @@
-SELECT COUNT(*)::INTEGER
-FROM AlertMessageWindow
-WHERE TenantId=@TenantId
-  AND Status=@Status
-  AND OccurredAtUtc >= @OccurredAtUtc - make_interval(mins => @WindowMinutes)
-  AND OccurredAtUtc <= @OccurredAtUtc
-  AND (@Provider IS NULL OR Provider=@Provider);
