35f72e13bc40487516d209217935ab7aba7c6c8e
heber mattos <feanorkun@hotmail.com>
2026-09-21 13:32:07 -0300

    Remove legacy platform administrator persistence

--- src/Sms.Infrastructure/Sql/Persistence/AdministratorRepository.CreateAsync.02.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO PlatformAdministrators
-    (Id, Username, Email, PasswordHash, PasswordSalt, PasswordIterations, IsActive, CreatedAt)
-VALUES
-    (@Id, @Username, @Email, @PasswordHash, @PasswordSalt, @PasswordIterations, @IsActive, CURRENT_TIMESTAMP);
