b60c3155a5fc6c201d2b5b717fd956c6c0e052f3
heber mattos <feanorkun@hotmail.com>
2026-09-21 19:14:00 -0300

    Fix seed tenant cache construction

--- database/seeds/Sms.Seed/Program.cs
+++ database/seeds/Sms.Seed/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging.Abstractions;
 using Sms.Infrastructure.Persistence;
+using Sms.Infrastructure.Caching;
 using Sms.Infrastructure.Security;
 using Sms.Seed;
 using Sms.Application.Auth;
@@ -35,10 +36,10 @@ if (await portalUsers.GetActiveByUsernameAsync(username.Trim(), "platform", null
     }
 }
 Console.WriteLine("Initial platform administrator is configured. Existing passwords are not overwritten.");
-var configurationCache = new TenantConfigurationCache(
-    connectionFactory,
+var distributedCache = new ResilientDistributedCache(
     new SeedDistributedCache(),
-    NullLogger<TenantConfigurationCache>.Instance);
+    NullLogger<ResilientDistributedCache>.Instance);
+var configurationCache = new TenantConfigurationCache(connectionFactory, distributedCache);
 var providers = new TenantSmsProviderRepository(
     connectionFactory,
     new AesGcmSecretProtector(configuration),
