b80967e697838d9823f58728cf9647839464637d
heber mattos <feanorkun@hotmail.com>
2026-09-21 13:13:28 -0300

    Make tenant settings cache resilient (#325)
    * Make rate limit settings cache resilient
    
    * Make AI settings cache resilient
    
    * Update resilient cache test construction

--- tests/Sms.Infrastructure.Tests/AdministrationSqlTests.cs
+++ tests/Sms.Infrastructure.Tests/AdministrationSqlTests.cs
@@ -2,6 +2,7 @@ using System.Security.Cryptography;
 using Dapper;
 using Npgsql;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
 using Sms.Application.Administration;
 using Sms.Application.Auth;
 using Sms.Application.Providers;
@@ -29,7 +30,7 @@ public sealed class AdministrationSqlTests
         var configurationCache = TenantConfigurationCacheTestFactory.Create(factory);
         var repository = new AdministrationRepository(factory, protector, configurationCache);
         var providers = new TenantSmsProviderRepository(factory, protector, configurationCache);
-        var service = new AdministrationService(repository, providers, [new TwilioSettingsPolicy(), new BandwidthSettingsPolicy()], new AdministrationServiceTests.TestProviderCatalogCache(), new TenantRateLimitRepository(factory, new TenantConfigurationCacheTestFactory.TestDistributedCache()));
+        var service = new AdministrationService(repository, providers, [new TwilioSettingsPolicy(), new BandwidthSettingsPolicy()], new AdministrationServiceTests.TestProviderCatalogCache(), new TenantRateLimitRepository(factory, new TenantConfigurationCacheTestFactory.TestDistributedCache(), NullLogger<TenantRateLimitRepository>.Instance));
         var credentials = new ApiClientRepository(factory, configurationCache);
         var tenant = Guid.NewGuid(); var other = Guid.NewGuid(); var account = Guid.NewGuid().ToString("N");
         using var connection = new NpgsqlConnection(configuration.GetConnectionString("Postgres"));
