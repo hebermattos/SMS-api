2ad3d9767f40484760fd4614c0449abf22ae4dda
heber mattos <feanorkun@hotmail.com>
2026-09-22 18:46:27 -0300

    Mark AdministrationSqlTests as integration tests

--- tests/TextRelay.Infrastructure.Tests/AdministrationSqlTests.cs
+++ tests/TextRelay.Infrastructure.Tests/AdministrationSqlTests.cs
@@ -14,6 +14,7 @@ using Sms.Infrastructure.Security;
 namespace Sms.Infrastructure.Tests;
 
 [Collection(PostgresTestCollection.Name)]
+[Trait("Category", "Integration")]
 public sealed class AdministrationSqlTests
 {
     [ReportingSqlFact]
